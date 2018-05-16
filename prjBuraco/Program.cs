76550080aaac94003fc785897319fd4c672b7d44
Rafael Gomes <rafafg6@hotmail.com>
2018-05-16 00:38:43 -0300

    Implemented gerarCartas() method

--- prjBuraco/Program.cs
+++ prjBuraco/Program.cs
@@ -8,20 +8,42 @@ namespace prjBuraco
     class Program
     {
 
-        string[] cartasDoJogo = new string[52];
-        string[,] cartasDaMao = new string[11, 4];
+        static string[] cartasDoJogo = new string[104];
+        static string[,] cartasDaMao = new string[11, 4];
 
         static void Main(string[] args)
         {
 
-            
+            gerarCartas();
+
+            Console.ReadKey();
 
         }
 
         static void gerarCartas()
         {
 
+            int cont = 0;
+            
+            for (int i = 1; i <= 2; i++)
+            {
 
+                for (int j = 1; j <= 4; j++)
+                {
+
+                    for (int k = 1; k <= 13; k++)
+                    {
+
+                        cartasDoJogo[cont] = String.Format("{0}{1:00}{2}", j, k, i);
+
+                        Console.Write(cartasDoJogo[cont] + " ");
+                        cont++;
+
+                    }
+
+                }
+
+            }
 
 
         }
